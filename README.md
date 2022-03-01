e71594d8b23cd60601fe72cb0e1e268fbbc6e575
José Duarte <duarte.gmj@gmail.com>
2022-03-01 21:00:54 +0000

    add basic commands

---
+++ README.md
@@ -0,0 +1,4 @@
+# Seshat
+
+> Seshat, was the ancient Egyptian goddess of wisdom, knowledge, and writing.
+> She was seen as a scribe and record keeper.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
+use clap::{Parser, Subcommand};
+
+pub fn default_config_path() -> String {
+    let home = std::env::var("HOME").unwrap();
+    return home + "/.config/seshat.toml";
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    commands: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Watch {
+        /// The directory to watch over.
+        directory: String,
+        /// Whether to watch sub-directories.
+        #[clap(short, long)]
+        recursive: bool,
+        /// Path to the configuration file.
+        #[clap(short, long, default_value_t=default_config_path())]
+        config: String,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
