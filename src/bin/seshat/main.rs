6d3b3d1ba03f980a70f5c8edba11f0030c058540
José Duarte <duarte.gmj@gmail.com>
2022-03-03 20:09:52 +0000

    file refactor

--- src/bin/seshat/main.rs
+++ src/bin/seshat/main.rs
@@ -1,25 +1,8 @@
-use seshat::{fs::list_subdirs, git::WatchedRepository};
+mod watch;
 
-use std::{
-    collections::HashSet,
-    env::current_dir,
-    ffi::OsStr,
-    sync::mpsc::{channel, Receiver, Sender},
-    time::Duration,
-};
+use watch::{Watch, WatchArgs};
 
-use clap::{Args, Parser, Subcommand};
-use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
-
-const DEFAULT_DELAY: u64 = 5;
-
-fn default_config_path() -> String {
-    std::env::var("HOME").unwrap() + "/.config/seshat.toml"
-}
-
-fn current_dir_string() -> String {
-    String::from(current_dir().unwrap().to_str().unwrap())
-}
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 struct Cli {
@@ -29,104 +12,8 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Watch over a given directory
     Watch(WatchArgs),
-}
-
-#[derive(Args)]
-struct WatchArgs {
-    /// The directory to watch over.
-    #[clap(default_value_t=current_dir_string())]
-    directory: String,
-    /// Whether to watch sub-directories.
-    #[clap(short, long)]
-    recursive: bool,
-    /// Path to the configuration file.
-    #[clap(short, long, default_value_t=default_config_path())]
-    config: String,
-    /// Watch over directory and print commands not performing them.
-    #[clap(long)]
-    dry_run: bool,
-    #[clap(long, default_value_t=DEFAULT_DELAY)]
-    delay: u64,
-}
-
-macro_rules! handle_event {
-    ($path:ident, $message:literal) => {
-        (
-            $path,
-            format!($message, $path.to_str().unwrap(), chrono::Utc::now()),
-        )
-    };
-}
-
-struct Watch {
-    args: WatchArgs,
-    repo: WatchedRepository,
-}
-
-impl From<WatchArgs> for Watch {
-    fn from(args: WatchArgs) -> Self {
-        let directory = args.directory.clone();
-        Self {
-            args,
-            repo: WatchedRepository::new(directory).unwrap(),
-        }
-    }
-}
-
-impl Watch {
-    fn run(self) {
-        let (tx, rx): (Sender<DebouncedEvent>, Receiver<DebouncedEvent>) = channel();
-        let mut watcher = watcher(tx, Duration::from_secs(self.args.delay)).unwrap();
-
-        let directories = list_subdirs(
-            &self.args.directory,
-            HashSet::from_iter(vec![".obsidian", ".git"].into_iter().map(|s| OsStr::new(s))),
-        );
-
-        for dir in &directories {
-            watcher.watch(dir, RecursiveMode::NonRecursive).unwrap();
-        }
-
-        println!("Watching over {:#?}", directories);
-
-        loop {
-            match rx.recv() {
-                Ok(event) => self.handle_event(&event, &self.repo),
-                Err(e) => println!("watch error: {:?}", e),
-            }
-        }
-    }
-
-    fn handle_event(&self, event: &DebouncedEvent, repo: &WatchedRepository) {
-        println!("{:?}", event);
-        // TODO: better commit messages (e.g. short title, descriptive body)
-        // TODO: configurable commit messages
-        let (path, message) = match event {
-            DebouncedEvent::Create(path) => handle_event!(path, "created file {} @ {}"),
-            DebouncedEvent::Write(path) => handle_event!(path, "written file {} @ {}"),
-            DebouncedEvent::Chmod(path) => handle_event!(path, "chmod file {} @ {}"),
-            DebouncedEvent::Remove(path) => handle_event!(path, "deleted file {} @ {}"),
-            DebouncedEvent::Rename(old, new) => (
-                new,
-                format!(
-                    "renamed file {} to {} @ {}",
-                    old.to_str().unwrap(),
-                    new.to_str().unwrap(),
-                    chrono::Utc::now()
-                ),
-            ),
-            // TODO: handle these two later
-            DebouncedEvent::Rescan => todo!(),
-            DebouncedEvent::Error(_, _) => todo!(),
-            DebouncedEvent::NoticeRemove(_) | DebouncedEvent::NoticeWrite(_) => {
-                return;
-            }
-        };
-
-        repo.stage(path).unwrap();
-        repo.commit(&message).unwrap();
-    }
 }
 
 fn main() {
---
+++ src/bin/seshat/watch.rs
@@ -0,0 +1,118 @@
+use seshat::{fs::list_subdirs, git::WatchedRepository};
+
+use std::{
+    collections::HashSet,
+    env::current_dir,
+    ffi::OsStr,
+    sync::mpsc::{channel, Receiver, Sender},
+    time::Duration,
+};
+
+use clap::Args;
+use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+const DEFAULT_DELAY: u64 = 5;
+
+fn default_config_path() -> String {
+    std::env::var("HOME").unwrap() + "/.config/seshat.toml"
+}
+
+fn current_dir_string() -> String {
+    String::from(current_dir().unwrap().to_str().unwrap())
+}
+
+#[derive(Args)]
+pub(crate) struct WatchArgs {
+    /// The directory to watch over.
+    #[clap(default_value_t=current_dir_string())]
+    directory: String,
+    /// Whether to watch sub-directories.
+    #[clap(short, long)]
+    recursive: bool,
+    /// Path to the configuration file.
+    #[clap(short, long, default_value_t=default_config_path())]
+    config: String,
+    /// Watch over directory and print commands not performing them.
+    #[clap(long)]
+    dry_run: bool,
+    #[clap(long, default_value_t=DEFAULT_DELAY)]
+    delay: u64,
+}
+
+macro_rules! handle_event {
+    ($path:ident, $message:literal) => {
+        (
+            $path,
+            format!($message, $path.to_str().unwrap(), chrono::Utc::now()),
+        )
+    };
+}
+
+pub(crate) struct Watch {
+    args: WatchArgs,
+    repo: WatchedRepository,
+}
+
+impl From<WatchArgs> for Watch {
+    fn from(args: WatchArgs) -> Self {
+        let directory = args.directory.clone();
+        Self {
+            args,
+            repo: WatchedRepository::new(directory).unwrap(),
+        }
+    }
+}
+
+impl Watch {
+    pub fn run(self) {
+        let (tx, rx): (Sender<DebouncedEvent>, Receiver<DebouncedEvent>) = channel();
+        let mut watcher = watcher(tx, Duration::from_secs(self.args.delay)).unwrap();
+
+        let directories = list_subdirs(
+            &self.args.directory,
+            HashSet::from_iter(vec![".obsidian", ".git"].into_iter().map(|s| OsStr::new(s))),
+        );
+
+        for dir in &directories {
+            watcher.watch(dir, RecursiveMode::NonRecursive).unwrap();
+        }
+
+        println!("Watching over {:#?}", directories);
+
+        loop {
+            match rx.recv() {
+                Ok(event) => self.handle_event(&event, &self.repo),
+                Err(e) => println!("watch error: {:?}", e),
+            }
+        }
+    }
+
+    fn handle_event(&self, event: &DebouncedEvent, repo: &WatchedRepository) {
+        println!("{:?}", event);
+        // TODO: better commit messages (e.g. short title, descriptive body)
+        // TODO: configurable commit messages
+        let (path, message) = match event {
+            DebouncedEvent::Create(path) => handle_event!(path, "created file {} @ {}"),
+            DebouncedEvent::Write(path) => handle_event!(path, "written file {} @ {}"),
+            DebouncedEvent::Chmod(path) => handle_event!(path, "chmod file {} @ {}"),
+            DebouncedEvent::Remove(path) => handle_event!(path, "deleted file {} @ {}"),
+            DebouncedEvent::Rename(old, new) => (
+                new,
+                format!(
+                    "renamed file {} to {} @ {}",
+                    old.to_str().unwrap(),
+                    new.to_str().unwrap(),
+                    chrono::Utc::now()
+                ),
+            ),
+            // TODO: handle these two later
+            DebouncedEvent::Rescan => todo!(),
+            DebouncedEvent::Error(_, _) => todo!(),
+            DebouncedEvent::NoticeRemove(_) | DebouncedEvent::NoticeWrite(_) => {
+                return;
+            }
+        };
+
+        repo.stage(path).unwrap();
+        repo.commit(&message).unwrap();
+    }
+}
