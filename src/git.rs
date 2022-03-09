f84a0143bb0af0f1bc459a8926315ad115ad6b6f
José Duarte <duarte.gmj@gmail.com>
2022-03-09 20:15:07 +0000

    fix(#18): push on exit

--- src/git.rs
+++ src/git.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use git2::{IndexAddOption, PushOptions};
+use log::error;
 
 type Result<T> = std::result::Result<T, git2::Error>;
 
@@ -63,21 +64,34 @@ impl WatchedRepository {
         Ok(())
     }
 
-    #[allow(unused)] // TODO: use this function when closing the program
+    /// Pushes the current branch into "origin".
+    /// The function relies on `ssh-agent` for git authentication.
     pub fn push(&self) -> Result<()> {
         let repo = &self.0;
+
+        let mut remote = repo.find_remote("origin")?;
+
         let head = repo.head()?;
-        let current_branch_name = head.name().unwrap();
-        let mut remote = repo.find_remote(&current_branch_name)?;
+        let refspecs: &[&str] = &[&head.name().unwrap()];
 
         let mut remote_callbacks = git2::RemoteCallbacks::new();
-        remote_callbacks.push_update_reference(|refname, status| {
-            println!("{:#?} {:#?}", refname, status);
-            Ok(())
+
+        remote_callbacks.credentials(|_url, username_from_url, _allowed_types| {
+            git2::Cred::ssh_key_from_agent(username_from_url.unwrap())
         });
 
-        let refspecs: &[&str] = &[];
+        remote_callbacks.push_update_reference(|refname, status| {
+            if let Some(status_message) = status {
+                error!("error pushing reference {}", refname);
+                Err(git2::Error::from_str(status_message))
+            } else {
+                Ok(())
+            }
+        });
+
         let mut push_options = PushOptions::new();
+        push_options.remote_callbacks(remote_callbacks);
+
         remote.push(refspecs, Some(&mut push_options))?;
         Ok(())
     }
